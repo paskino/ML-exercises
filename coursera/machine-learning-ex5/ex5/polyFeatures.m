bf19740fc99fb6be5e0d81b44e30c0be926069d7
paskino <edo.paskino@gmail.com>
2018-04-15 11:16:12 +0200

    added polyfeatures

--- coursera/machine-learning-ex5/ex5/polyFeatures.m
+++ coursera/machine-learning-ex5/ex5/polyFeatures.m
@@ -14,11 +14,18 @@ X_poly = zeros(numel(X), p);
 %               column of X contains the values of X to the p-th power.
 %
 % 
+%m = size(X,1);
+%for j=1:m
+%  X_poly(j,1) = X(1);
+%  for i=2:p
+%   X_poly(j,i) = X(i) * X_poly(i-1);
+%  endfor
+%endfor
 
-%for i=1:p
-
-
-
+X_poly(:,1) = X;
+for i=2:p
+  X_poly(:,i) = X .* X_poly(:,i-1);
+endfor
 
 % =========================================================================
 
