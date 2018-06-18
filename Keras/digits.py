13faca8ef28e4323532eb8b300188e465e6b676e
Edoardo Pasca <edo.paskino@gmail.com>
2018-06-18 14:01:21 +0000

    first quite working keras example

--- Keras/digits.py
+++ Keras/digits.py
@@ -2,38 +2,69 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
 import numpy
 import scipy.io as sio
+import keras
+from keras.layers import Dense, Input
+from keras.models import Sequential
+from keras import backend as K
+import matplotlib.pyplot as plt
 
-tf.logging.set_verbosity(tf.logging.INFO)
+# Simple Sequential Dense Neural Network
 
+model = Sequential()
+#https://github.com/keras-team/keras/issues/4813
+reg = keras.regularizers.l2(0.01)
+model.add(Dense(25, activation=keras.activations.sigmoid, 
+                input_dim=400,
+                kernel_regularizer=reg))
+model.add(Dense(10, 
+                activation=keras.activations.sigmoid,
+                kernel_regularizer=reg))
+                
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+## input dataset
 filename = ("../coursera/machine-learning-ex4/ex4/ex4data1.mat")
 
 data = sio.loadmat(filename)
 
-X = data['X']
-Y = data['y']
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Dense(400, activation="relu", input_shape=(400,)) ,
-  tf.keras.layers.Dense(25, activation="relu") ,
-  tf.keras.layers.Dense(10, activation="relu") ,
+# the dataset is sorted, so limiting to 4000 won't train on certain numbers
+# let's shuffle it
+order = numpy.random.permutation(range(len(data['X'])))
+X = data['X'][order]
+Y = data['y'][order]
 
-])
+ts = 4000
 
-def loss(model, x, y):
-  y_ = model(x)
-  return tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_)
+train_datasetX = X[:ts]
+train_datasetY = numpy.zeros(( ts , 10)) 
+for i in range(ts):
+    j = Y[i]
+    if Y[i] == 10:
+        j = 0
+    train_datasetY[i][ j ] = 1
+
+test_datasetX = X[ts:]
+test_datasetY = numpy.zeros((len(X)-ts, 10)) 
+for i in range(len(X)-ts):
+    j = Y[i+ts]
+    if Y[i+ts] == 10:
+        j = 0
+    test_datasetY[i][ j ] = 1
 
 
-def grad(model, inputs, targets):
-  with tf.GradientTape() as tape:
-    loss_value = loss(model, inputs, targets)
-  return tape.gradient(loss_value, model.variables)
+hist = model.fit(train_datasetX, train_datasetY, epochs=50)
+print (hist.history)
+score = model.evaluate(test_datasetX, test_datasetY)
 
-optimiser = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-
-train_datasetX = X[:4000]
-train_datasetY = Y[:4000]
-
+vis = 3
+p = model.predict(train_datasetX[0:vis])
+py = numpy.zeros(numpy.shape(p)) 
+for i in range(vis):
+    py[i][numpy.argmax(p[i])] = 1
